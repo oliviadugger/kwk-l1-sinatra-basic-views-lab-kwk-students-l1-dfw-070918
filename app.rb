bf231e15ab9814e2b8bd9a2c48301c163b517cbe
oliviadugger <oliviacdugger@gmail.com>
2018-07-17 18:44:02 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,6 +2,6 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/bowling-name' do
-  "<h1> Buster's Best Bowling Alley </h1>"
+  <h1> "Buster's Best Bowling Alley" </h1>
   end
 end
